e149e43abccd004fdc22bd3ad6db828d71db1cce
Moises Hernandez Sanchez <telekineticjake@gmail.com>
2022-06-23 15:37:19 -0400

    Fixed sensor error, added more cars

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const carCtx = carCanvas.getContext('2d')
 const networkCtx = networkCanvas.getContext('2d')
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9)
 
-const N = 1
+const N = 50
 const cars = generateCars(N)
 let bestCar = cars[0]
 if (localStorage.getItem('bestBrain')) {
@@ -30,6 +30,10 @@ const traffic = [
   new Car(road.getLaneCenter(1), -500, 30, 50, 'DUMMY', 2, getRandomColor()),
   new Car(road.getLaneCenter(1), -700, 30, 50, 'DUMMY', 2, getRandomColor()),
   new Car(road.getLaneCenter(2), -700, 30, 50, 'DUMMY', 2, getRandomColor()),
+  new Car(road.getLaneCenter(0), -900, 30, 50, 'DUMMY', 2, getRandomColor()),
+  new Car(road.getLaneCenter(2), -900, 30, 50, 'DUMMY', 2, getRandomColor()),
+  new Car(road.getLaneCenter(1), -1100, 30, 50, 'DUMMY', 2, getRandomColor()),
+  new Car(road.getLaneCenter(2), -1100, 30, 50, 'DUMMY', 2, getRandomColor()),
 ]
 
 animate()
@@ -67,15 +71,15 @@ function animate(time) {
 
   road.draw(carCtx)
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(carCtx, 'red')
+    traffic[i].draw(carCtx)
   }
 
   carCtx.globalAlpha = 0.2
   for (let i = 0; i < cars.length; i++) {
-    cars[i].draw(carCtx, 'blue')
+    cars[i].draw(carCtx)
   }
   carCtx.globalAlpha = 1
-  bestCar.draw(carCtx, 'blue', true)
+  bestCar.draw(carCtx, true)
 
   carCtx.restore()
 
